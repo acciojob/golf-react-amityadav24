15bef46177dd391f81101b30d9568218f12e5b94
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-07-04 13:39:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,59 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import './App.css';
 
-class GolfGame extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showBall: false,    // false = show Start button; true = show ball
-      ballLeft: 0,        // ball horizontal position in pixels
+      ballVisible: false,
+      ballPosition: 0,
     };
   }
 
+  buttonClickHandler = () => {
+    this.setState({ ballVisible: true });
+  };
+
   componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
+    window.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
+    window.removeEventListener('keydown', this.handleKeyDown);
   }
 
   handleKeyDown = (event) => {
-    // Only move ball if it is shown and Right Arrow key pressed
-    if (this.state.showBall && (event.keyCode === 39 || event.key === "ArrowRight")) {
+    if (event.keyCode === 39) { // Right Arrow key
       this.setState((prevState) => ({
-        ballLeft: prevState.ballLeft + 5,
+        ballPosition: prevState.ballPosition + 5,
       }));
     }
   };
 
-  buttonClickHandler = () => {
-    // Hide start button and show ball, reset position
-    this.setState({ showBall: true, ballLeft: 0 });
-  };
-
   renderChoice() {
-    if (!this.state.showBall) {
-      // Show Start button initially
-      return (
-        <button className="start" onClick={this.buttonClickHandler}>
-          Start
-        </button>
-      );
-    } else {
-      // Show ball when game started
+    if (this.state.ballVisible) {
       return (
         <div
           className="ball"
-          style={{
-            position: "absolute",
-            left: this.state.ballLeft + "px",
-            top: "50%",
-            transform: "translateY(-50%)",
-            width: "50px",
-            height: "50px",
-            borderRadius: "50%",
-            backgroundColor: "green",
-          }}
-        />
+          style={{ position: 'absolute', left: this.state.ballPosition + 'px', top: '50%' }}
+        ></div>
+      );
+    } else {
+      return (
+        <button className="start" onClick={this.buttonClickHandler}>
+          Start Game
+        </button>
       );
     }
   }
 
   render() {
     return (
-      <div style={{ position: "relative", height: "100vh" }}>
+      <div className="App">
         {this.renderChoice()}
       </div>
     );
   }
 }
 
-export default GolfGame;
+export default App;
