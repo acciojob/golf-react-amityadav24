912ca252cf8a880ab55df1ccbb4146a45451183c
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-07-04 13:29:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import ".styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,9 +22,14 @@ class App extends Component {
       this.state.gameStarted &&
       (event.key === "ArrowRight" || event.keyCode === 39)
     ) {
-      this.setState((prevState) => ({
-        ballPosition: prevState.ballPosition + 5,
-      }));
+      this.setState((prevState) => {
+        // Limit ball movement so it doesn't go off screen (optional)
+        const maxRight = window.innerWidth - 60; // ball width + some padding
+        const nextPos = prevState.ballPosition + 5;
+        return {
+          ballPosition: nextPos > maxRight ? maxRight : nextPos,
+        };
+      });
     }
   };
 
@@ -39,9 +44,28 @@ class App extends Component {
     const { gameStarted, ballPosition } = this.state;
 
     return (
-      <div style={{ position: "relative", height: "100vh" }}>
+      <div
+        style={{
+          position: "relative",
+          height: "100vh",
+          backgroundColor: "white",
+          overflow: "hidden",
+        }}
+      >
         {!gameStarted && (
-          <button className="start" onClick={this.buttonClickHandler}>
+          <button
+            className="start"
+            onClick={this.buttonClickHandler}
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              padding: "15px 30px",
+              fontSize: "1.2rem",
+              cursor: "pointer",
+            }}
+          >
             Start
           </button>
         )}
@@ -59,6 +83,7 @@ class App extends Component {
               borderRadius: "50%",
               backgroundColor: "green",
               transition: "left 0.1s ease",
+              boxShadow: "0 0 8px 2px rgba(0, 128, 0, 0.5)",
             }}
           />
         )}
